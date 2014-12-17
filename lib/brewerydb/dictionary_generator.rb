4eb368939e3581fe0542fb7a2e284a77f53d7452
Ronald Hong <rhong@fiksu.com>
2014-12-17 14:56:38 -0800

    Add dictionary generator.

---
+++ lib/brewerydb/dictionary_generator.rb
@@ -0,0 +1,9 @@
+# require stuff
+
+client = BreweryDB::Client.new do |config|
+  config.api_key = Rails.application.secrets.brewery_db_api_key
+end
+
+beers = client.beers.all.as_json
+
+File.new("#{File.dirname}/dictionary.json", 'w').write(beers.map { |b| b['name'] }.to_json)
